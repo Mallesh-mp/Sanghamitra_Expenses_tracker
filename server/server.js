a128a3ae555c22488c08440cd14af97aa997a731
Mallesh-mp <malleshkurga48@gmail.com>
2025-08-15 09:19:27 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,32 @@
+// server.js
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+
+const expensesRoutes = require("./routes/expenseRoutes");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/expenses", expensesRoutes);
+
+// Test route to check server connection
+app.get("/api/test", (req, res) => {
+  res.json({ message: "✅ Server is running fine!" });
+});
+
+// DB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ Mongo error:", err));
+
+// Server start
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
